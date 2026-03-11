3ad1eb079e3f02d785b20293c5ed520a429b8f54
Rohit <01rohitkumar0104@gmail.com>
2026-03-12 00:02:39 +0530

    Fixed deployment

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,22 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'three-vendor': ['three', '@react-three/fiber', '@react-three/drei'],
+          'react-vendor': ['react', 'react-dom'],
+          'utils-vendor': ['papaparse']
+        }
+      }
+    }
+  },
+  server: {
+    port: 5173,
+    host: true
+  }
 })
